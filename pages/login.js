e5a7f950dc4659de2a19cf152c604c5737569293
lu15v <lu15bytes@gmail.com>
2020-08-30 23:28:15 -0700

    ADDED: login and newaccount pages

---
+++ pages/login.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Layout from '../components/Layout';
+
+const Login = () => {
+    return(
+        <Layout>
+            <h1 className="text-center text-2xl text-white font-light">login</h1>
+        </Layout>
+    );
+}
+
+
+export default Login;
