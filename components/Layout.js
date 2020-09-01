640b3ef378c2237bbacc7401f7e7e9d898ab9b7b
lu15v <lu15bytes@gmail.com>
2020-08-31 23:45:38 -0700

    FIX: typo

--- components/Layout.js
+++ components/Layout.js
@@ -11,7 +11,7 @@ const Layout = ({children}) => {
         <>
             <Head>
                 <title>CRM ADMIN</title>
-                <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.css" integrity="sha512-oHDEc8Xed4hiW6CxD7qjbnI+B07vDdX7hEPTvn9pSZO1bcRqHp8mj9pyr+8RVC2GmtEfI2Bi9Ke9Ass0as+zpg==" crossorigin="anonymous" />
+                <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.css" integrity="sha512-oHDEc8Xed4hiW6CxD7qjbnI+B07vDdX7hEPTvn9pSZO1bcRqHp8mj9pyr+8RVC2GmtEfI2Bi9Ke9Ass0as+zpg==" crossOrigin="anonymous" />
                 <link href="https://unpkg.com/tailwindcss@^1.0/dist/tailwind.min.css" rel="stylesheet"/>
             </Head>
             {router.pathname === "/login" || router.pathname === "/newAccount" ? (
